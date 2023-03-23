d2e5faf2924f92bb1bfa5102f15e96f356170853
Emildore <Emily.tran.1995@gmail.com>
2023-03-22 19:30:03 -0700

    Add Screen Border

--- src/main/java/ca/bcit/comp2522/termproject/graphicalrpg/Player.java
+++ src/main/java/ca/bcit/comp2522/termproject/graphicalrpg/Player.java
@@ -2,25 +2,32 @@ package ca.bcit.comp2522.termproject.graphicalrpg;
 
 import javafx.scene.input.KeyCode;
 
+import static ca.bcit.comp2522.termproject.graphicalrpg.main.*;
+
 public class Player extends Entity {
 
     private boolean leftPressed, rightPressed, upPressed, downPressed;
+    private int minX, maxX, minY, maxY;
 
     public Player(String string) {
         super(string);
+        this.minX = 0;
+        this.maxX = screenWidth - tileSize;
+        this.minY = 0;
+        this.maxY = screenHeight - tileSize;
     }
 
     public void update() {
-        if (leftPressed) {
+        if (leftPressed && posx > minX) {
             posx -= 5;
         }
-        if (rightPressed) {
+        if (rightPressed && posx < maxX) {
             posx += 5;
         }
-        if (upPressed) {
+        if (upPressed && posy > minY) {
             posy -= 5;
         }
-        if (downPressed) {
+        if (downPressed && posy < maxY) {
             posy += 5;
         }
     }
