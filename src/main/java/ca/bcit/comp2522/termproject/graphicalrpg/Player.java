4036fc607579dca1b0fcf18eaa2d97099ab63052
SSollestre <sean.sollestre@gmail.com>
2023-03-30 18:50:15 -0700

    Add combat helper functions to Player and Entity

--- src/main/java/ca/bcit/comp2522/termproject/graphicalrpg/Player.java
+++ src/main/java/ca/bcit/comp2522/termproject/graphicalrpg/Player.java
@@ -1,11 +1,21 @@
 package ca.bcit.comp2522.termproject.graphicalrpg;
 
+import java.util.ArrayList;
+
 public class Player extends Entity {
 
     private double minX = -300;
     private double maxX = 370;
     private double minY = -300;
     private double maxY = 60;
+
+    // Combat
+    private int level;
+    private boolean win;
+    private boolean lose;
+    private int expThreshold;
+    private Entity target;
+    private ArrayList<String> skills;
 
     private double posX;
     private double posY;
@@ -83,4 +93,103 @@ public class Player extends Entity {
         super.haveWon();
     }
 
+    public void setWin(boolean win) {
+        this.win = win;
+    }
+
+    public void setLose(boolean lose) {
+        this.lose = lose;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public boolean getWin() {
+        return win;
+    }
+
+    public boolean getLose() {
+        return lose;
+    }
+
+    @Override
+    public float attack() {
+        return this.getCurrATK();
+    }
+
+    public float doubleAttack() {
+        return this.attack() * 2;
+    }
+
+    public double block() {
+        return this.getBaseHP() * 0.5;
+    }
+
+    public int getExpThreshold() {
+        return expThreshold;
+    }
+
+    public void setExpThreshold(int expThreshold) {
+        this.expThreshold = expThreshold;
+    }
+
+    public void levelUp() {
+        this.level += 1;
+        if (this.level == 2) {
+            skills.add("2");
+        } else if (this.level == 3) {
+            skills.add("3");
+        }
+    }
+
+    public void displaySkills() {
+        System.out.println("***\nMoves");
+        if (this.getLevel() >= 1) {
+            System.out.println("1: Attack");
+        }
+        if (this.getLevel() >= 2) {
+            System.out.println("2: Power Strike");
+        }
+        if (this.getLevel() >= 3) {
+            System.out.println("3: Block");
+        }
+    }
+
+    public void interact(String selection, Entity nTarget) {
+        if (selection.equals("1")) {
+            target = nTarget;
+            if (target != null) {
+                target.receiveDMG(this.getCurrATK());
+                System.out.println(target.getName() + ": " + target.getCurrHP());
+            }
+        }
+        if (selection.equals("2")) {
+            target = nTarget;
+            target.receiveDMG(this.doubleAttack());
+            System.out.println(target.getName() + ": " + target.getCurrHP());
+        }
+
+        if (selection.equals("3")) {
+            this.addCurrHP((float) block());
+        }
+    }
+
+
+    public void checkLevelUp() {
+        if (this.getExp() >= this.getExpThreshold()) {
+            this.levelUp();
+            System.out.println("level up!\nPlayer Level: " + this.getLevel());
+            if (this.getExp() > this.getExpThreshold()) {
+                this.setExp(this.getExp() - this.getExpThreshold());
+                this.checkLevelUp();
+            } else {
+                this.setExp(0);
+            }
+        }
+    }
+
+    public ArrayList<String> getSkills() {
+        return skills;
+    }
 }
